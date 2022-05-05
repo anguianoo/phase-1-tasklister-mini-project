d52ba501c3c3c7b7408c8fadfc31420aca90c5a6
anguianoo <njanguiano@gmail.com>
2022-05-04 18:03:29 -0700

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  // const description = document.getElementById("new-task-description");
+
+  form.addEventListener("submit", createNewTask);
 });
+
+const createTask = (e) => {
+  e.preventDefault();
+  console.log("createTask");
+  const task = document.createElement("li");
+  task.innerText = newTaskDescription.value;
+
+  appendTask(newTask);
+  e.target.reset();
+};
+
+const appendNewTask = (task) => {
+  const ulTask = document.getElementById("tasks");
+  ulTask.getElementById("tasks").appendChild(task);
+};
